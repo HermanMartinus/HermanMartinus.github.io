c7d1493d9bb69bce8ce775334461551414d02dba
Herman Martinus <hfbmartinus@gmail.com>
2019-05-22 15:53:56 +0000

    Updated projects

--- script.js
+++
@@ -1,6 +0,0 @@
-function SendMessage() {
-  const baseLink = "mailto:hfbmartinus@gmail.com";
-  const subject = document.querySelector("#subject").value;
-  const message = document.querySelector("#body").value;
-  window.open(baseLink + "?subject=" + subject + " [via hermanmartinus.github.io]&body=" + message, '_blank');
-}
